982936ebbe1bd2774a1c0eee5613c298fc35438b
ductrongT2207E <trongdd232@gmail.com>
2022-09-13 00:45:37 +0700

    up code

---
+++ js/assignmentjs3.js
@@ -0,0 +1,28 @@
+function update(city){
+    var bg = document.getElementById("bg-fade");
+    bg.style.display = "block";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function (){
+        // console.log(this.responseText);
+        //convert to object of js
+        var data = JSON.parse(this.responseText);
+        console.log(data.main.temp); //26
+
+        var t = data.main.temp;
+        var x1 = document.getElementById("temp");
+        x1.innerText = t;
+
+        var n = data.name+", "+ data.sys.country;
+        var x2 = document.getElementById("city");
+        x2.innerText = n;
+
+        var p = data.main.pressure;
+        var x3 = document.getElementById("pressure");
+        x3.innerText = p;
+        bg.style.display = "none";
+
+    }
+    var link = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=09a71427c59d38d6a34f89b47d75975c&units=metric";
+    xhttp.open("get",link,true);
+    xhttp.send();
+}
