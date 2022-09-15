e23a5207d36a404b5d8728e290be83417db2ba7e
ductrongT2207E <trongdd232@gmail.com>
2022-09-15 21:57:48 +0700

    up code

--- js/assignmentjs2.js
+++ js/assignmentjs2.js
@@ -1,20 +1,17 @@
-
 function checkPrime(x){
-    //kiem tra x co phai so nguyen to hay khong
     if (x<2){
         return false;
-    } else if(x == 2 || x == 3){
+    } else if (x===2 || x===3){
         return true;
     } else {
         for(var i=2;i<=x/2;i++){
-            if (x%i==0){
+            if (x%i === 0){
                 return false;
             }
         }
         return true;
     }
 }
-
 function nextPrime(x){
     for (var i=x+1;true;i++){
         if(checkPrime(i)){
@@ -22,15 +19,9 @@ function nextPrime(x){
         }
     }
 }
-var n = 0;
-var m = 0;
+var n = 3;
 function countdown(){
     var xyz = document.getElementById("head");
-    xyz.innerText += " " +n;
+    xyz.innerText +=" "+n;
     n = nextPrime(n);
-    m++;
-    if(m>20){
-        clearInterval(si);
     }
-}
-var si= setInterval(countdown, 1000);
